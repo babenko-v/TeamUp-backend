310e2a7327cc86f4a5a2f28a513a4414b89495dd
babenko-v <11112004valerababenko@gmail.com>
2025-08-14 22:40:12 +0300

    Implement interface for Password Hasher

--- src/application/auth/interfaces.py
+++ src/application/auth/interfaces.py
@@ -14,3 +14,13 @@ class ITokenService(ABC):
     @abstractmethod
     def decode_token(self, token: str) -> Dict:
         pass
+
+class IPasswordHasher(ABC):
+
+    @abstractmethod
+    def verify_password(self, plain_password: str, hashed_password) -> bool:
+        pass
+
+    @abstractmethod
+    def get_password_hash(self, password: str) -> str:
+        pass
