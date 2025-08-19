cd59cc54fbbcc1428e5ba397f8a6ef5de257dea3
babenko-v <11112004valerababenko@gmail.com>
2025-08-20 00:46:45 +0300

    Replace mocked moments with IUserRepository

--- src/application/auth/service.py
+++ src/application/auth/service.py
@@ -1,18 +1,21 @@
 from application.auth.dto import LoginRequestDTO
 from application.auth.interfaces import ITokenService
+from application.shared.interfaces import IGenericRepository
+from application.users.interfaces import IUserRepository
 from infrastructure.auth.hashing import PasswordHasher
 
 
 class AuthService:
 
-    def __init__(self, token_service: ITokenService):
+    def __init__(self, token_service: ITokenService, user_repository: IUserRepository):
 
         self.token_service = token_service
         self.password_hasher = PasswordHasher()
+        self.user_repository = user_repository
 
     def login(self, login_data: LoginRequestDTO) -> tuple[str, str]:
 
-        user = ... # Mock user object, swap on User Repository method get by email
+        user = self.user_repository.get_user_by_email(login_data.email)
         if not user or not self.password_hasher.verify_password(login_data.password, user.hashed_password):
             raise ValueError("Incorrect password or email")
 
@@ -27,7 +30,7 @@ class AuthService:
 
         user_id = self.token_service.get_user_id_by_refresh_token(refresh_token)
 
-        user = ... # Mock user object, swap on User Repository method get by email
+        user = self.user_repository.get_by_id(user_id)
 
         if not user:
             raise ValueError("User were not found")
--- src/presentation/dependencies.py
+++ src/presentation/dependencies.py
@@ -1,9 +1,16 @@
+from typing import Optional
+
 from application.auth.interfaces import ITokenService
 from application.auth.service import AuthService
+from application.users.interfaces import IUserRepository
 from infrastructure.auth.jwt import JWTService
 
 from fastapi import Depends
 
+
+def get_user_repo() -> Optional[IUserRepository]:
+
+    return None     # Mocked return variable, swap on implementation User Repository
 
 def get_token_service() -> ITokenService:
 
@@ -11,6 +18,7 @@ def get_token_service() -> ITokenService:
 
 def get_auth_service(
     token_service: ITokenService = Depends(get_token_service),
+    user_repository: IUserRepository = Depends(get_user_repo),
 ) -> AuthService:
 
-    return AuthService(token_service=token_service)
+    return AuthService(token_service=token_service, user_repository=user_repository)
