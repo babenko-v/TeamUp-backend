9460c3ab2092822539422a268e4636f25b9deda0
babenko-v <11112004valerababenko@gmail.com>
2025-08-21 00:33:01 +0300

    Change basic config alembic for our database settings

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import os
+import sys
+
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.infrastructure.database.session import Base, DATABASE_URL
+
+from src.infrastructure.database.models import *
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -38,7 +47,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = DATABASE_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,6 +66,11 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+
+    config.set_main_option('sqlalchemy.url', DATABASE_URL)
+    target_metadata = Base.metadata
+
+
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
