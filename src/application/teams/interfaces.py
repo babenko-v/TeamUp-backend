2567df1f697c85d7f8c87cd10c3a2786ae953c8f
babenko-v <11112004valerababenko@gmail.com>
2025-09-12 21:51:20 +0300

    Add interface for Team Repository

---
+++ src/application/teams/interfaces.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from application.shared.interfaces import IGenericRepository
+from domain.models import Team, TeamMember
+
+
+class ITeamRepository(IGenericRepository[Team], ABC):
+
+    @abstractmethod
+    async def add_member(self, member: TeamMember) -> TeamMember | None:
+        pass
+
+    @abstractmethod
+    async def remove_member(self, member: TeamMember) -> TeamMember | None:
+        pass
+
+    @abstractmethod
+    async def get_team_by_name(self, name: str) -> Optional[Team]:
+        pass
+
+    @abstractmethod
+    async def exists_team_by_name(self, name: str) -> bool:
+        pass
+
