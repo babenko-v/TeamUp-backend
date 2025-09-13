299fc254257db8a9f45423850a290426fe28cf35
babenko-v <11112004valerababenko@gmail.com>
2025-09-13 16:04:46 +0300

    Add domain rules to patch method

--- src/domain/models.py
+++ src/domain/models.py
@@ -1,4 +1,5 @@
 import dataclasses
+import re
 import uuid
 from datetime import datetime
 from typing import Set, Dict, List
@@ -26,6 +27,40 @@ class User:
         if self.status_user == StatusUserEnum.BANNED:
             return
         self.status_user = StatusUserEnum.BANNED
+
+    def update(self, username: str | None, email: str | None,
+               avatar_url: str | None, linkedin_url: str | None, github_url: str | None,
+               platform_role: PlatformRoleEnum, status_user: StatusUserEnum):
+
+        if username is not None:
+            if len(username.strip()) < 3:
+                raise ValueError("Username must be at least 3 characters long.")
+            self.username = username.strip()
+
+        if avatar_url is not None:
+            if not avatar_url.startswith(('http://', 'https://')):
+                raise ValueError("Invalid avatar URL format.")
+            self.avatar_url = avatar_url
+
+        if email is not None:
+            valid = re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email)
+            if valid:
+                self.email = email
+            else:
+                raise ValueError("Invalid email address.")
+
+        if linkedin_url is not None:
+            self.linkedin_url = linkedin_url
+
+        if github_url is not None:
+            self.github_url = github_url
+
+        if platform_role is not None:
+            self.platform_role = platform_role
+
+        if status_user is not None:
+            self.status_user = status_user
+
 
 @dataclasses.dataclass(frozen=True)
 class TeamMember:
