224d34c6121808dedeffa91d1f3386d0c54b4b26
babenko-v <11112004valerababenko@gmail.com>
2025-09-18 23:56:38 +0300

    Implement realization of remove membet from the team in Team service

--- src/application/teams/services.py
+++ src/application/teams/services.py
@@ -2,6 +2,7 @@ import uuid
 
 from application.uow.interfaces import IUnitOfWork
 from domain.enum import TeamRoleEnum
+from domain.models import User as DomainUser
 
 
 class TeamService:
@@ -31,6 +32,30 @@ class TeamService:
             team.add_member(user_to_add, roles_new_user)
 
             await self.uow.teams.update(team)
+
+
+    async def remove_member(self, user_id_to_remove: uuid.UUID, team_id: uuid.UUID,
+                            current_user: DomainUser):
+
+        async with self.uow:
+            team = await self.uow.teams.get_by_id(team_id)
+            if team is None:
+                raise ValueError('Team not found')
+
+            is_leaving_myself = user_id_to_remove == current_user.id
+
+            access_to_remove_user = team.is_owner_or_maintainer(current_user.id)
+
+            if not is_leaving_myself or not access_to_remove_user:
+                raise ValueError("You don't have enough permission to remove user from team")
+
+            team.remove_member(user_id_to_remove)
+
+            await self.uow.teams.update(team)
+
+
+
+
 
 
 
