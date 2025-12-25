f33c4b2bee6e14cf9f355052b1d3b30e75648bd2
babenko-v <11112004valerababenko@gmail.com>
2025-12-25 16:40:55 +0200

    Add new DTO for batch method in Team app layer

--- src/application/teams/dto.py
+++ src/application/teams/dto.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import Optional, Set
+from typing import Optional, Set, List
 
 from pydantic import BaseModel, constr, Field
 
@@ -15,6 +15,20 @@ class UpdateTeamDTO(BaseModel):
     name: Optional[constr(min_length=3, max_length=50)] = None
     description: Optional[constr(min_length=10, max_length=500)] = None
     avatar_url: Optional[constr(min_length=10, max_length=500)] = None
+
+
+class AddMemberTeamDTO(BaseModel):
+    user_id: uuid.UUID
+    roles: Set[TeamRoleEnum] = Field(..., min_length=1)
+
+class BatchAddPMemberTeamDTO(BaseModel):
+    team_id: uuid.UUID
+    members: List[AddMemberTeamDTO]
+
+class BatchRemoveMemberTeamDTO(BaseModel):
+    team_id: uuid.UUID
+    user_ids: List[uuid.UUID]
+
 
 
 class AssignRoleDTO(BaseModel):
